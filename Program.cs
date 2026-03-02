835594f42bab5850966e216b6324c3df81b37dbb
joseph670615519 <joseph_dan@cmu.ac.th>
2026-03-02 16:09:09 +0700

    add files via upload - thread
    added thread to program

--- Program.cs
+++ Program.cs
@@ -39,10 +39,6 @@ namespace DNWS
         }
     }
 
-    /// <summary>
-    /// HTTP processor will process each http request
-    /// </summary>
-
     public class HTTPProcessor
     {
         protected class PluginInfo
@@ -53,49 +49,24 @@ namespace DNWS
             protected bool _postprocessing;
             protected IPlugin _reference;
 
-            public string path
-            {
-                get { return _path;}
-                set {_path = value;}
-            }
-            public string type
-            {
-                get { return _type;}
-                set {_type = value;}
-            }
-            public bool preprocessing
-            {
-                get { return _preprocessing;}
-                set {_preprocessing = value;}
-            }
-            public bool postprocessing
-            {
-                get { return _postprocessing;}
-                set {_postprocessing = value;}
-            }
-            public IPlugin reference
-            {
-                get { return _reference;}
-                set {_reference = value;}
-            }
+            public string path { get { return _path;} set {_path = value;} }
+            public string type { get { return _type;} set {_type = value;} }
+            public bool preprocessing { get { return _preprocessing;} set {_preprocessing = value;} }
+            public bool postprocessing { get { return _postprocessing;} set {_postprocessing = value;} }
+            public IPlugin reference { get { return _reference;} set {_reference = value;} }
         }
-        // Get config from config manager, e.g., document root and port
+
         protected string ROOT = Program.Configuration["DocumentRoot"];
         protected Socket _client;
         protected Program _parent;
         protected Dictionary<string, PluginInfo> plugins;
 
-        /// <summary>
-        /// Constructor, set the client socket and parent ref, also init stat hash
-        /// </summary>
-        /// <param name="client">Client socket</param>
-        /// <param name="parent">Parent ref</param>
         public HTTPProcessor(Socket client, Program parent)
         {
             _client = client;
             _parent = parent;
             plugins = new Dictionary<string, PluginInfo>();
-            // load plugins
+
             var sections = Program.Configuration.GetSection("Plugins").GetChildren();
             foreach(ConfigurationSection section in sections) {
                 PluginInfo pi = new PluginInfo();
@@ -108,27 +79,16 @@ namespace DNWS
             }
         }
 
-        /// <summary>
-        /// Get a file from local harddisk based on path
-        /// </summary>
-        /// <param name="path">Absolute path to the file</param>
-        /// <returns></returns>
         protected HTTPResponse getFile(String path)
         {
             HTTPResponse response = null;
 
-            // Guess the content type from file extension
             string fileType = "text/html";
             if (path.ToLower().EndsWith("jpg") || path.ToLower().EndsWith("jpeg"))
-            {
                 fileType = "image/jpeg";
-            }
             if (path.ToLower().EndsWith("png"))
-            {
                 fileType = "image/png";
-            }
 
-            // Try to read the file, if not found then 404, otherwise, 500.
             try
             {
                 response = new HTTPResponse(200);
@@ -146,12 +106,8 @@ namespace DNWS
                 response.body = Encoding.UTF8.GetBytes("<h1>500 Internal Server Error</h1>" + ex.Message);
             }
             return response;
-
         }
 
-        /// <summary>
-        /// Get a request from client, process it, then return response to client
-        /// </summary>
         public void Process()
         {
             NetworkStream ns = new NetworkStream(_client);
@@ -161,7 +117,6 @@ namespace DNWS
             byte[] bytes = new byte[1024];
             int bytesRead;
 
-            // Read all request
             do
             {
                 bytesRead = ns.Read(bytes, 0, bytes.Length);
@@ -171,75 +126,57 @@ namespace DNWS
             request = new HTTPRequest(requestStr);
             request.addProperty("RemoteEndPoint", _client.RemoteEndPoint.ToString());
 
-            // We can handle only GET now
             if(request.Status != 200) {
                 response = new HTTPResponse(request.Status);
             }
             else
             {
                 bool processed = false;
-                // pre processing
+
                 foreach(KeyValuePair<string, PluginInfo> plugininfo in plugins) {
-                    if(plugininfo.Value.preprocessing) {
+                    if(plugininfo.Value.preprocessing)
                         plugininfo.Value.reference.PreProcessing(request);
-                    }
                 }
-                // plugins
+
                 foreach(KeyValuePair<string, PluginInfo> plugininfo in plugins) {
                     if(request.Filename.StartsWith(plugininfo.Key)) {
                         response = plugininfo.Value.reference.GetResponse(request);
                         processed = true;
                     }
                 }
-                // local file
+
                 if(!processed) {
                     if (request.Filename.Equals(""))
-                    {
                         response = getFile(ROOT + "/index.html");
-                    }
                     else
-                    {
                         response = getFile(ROOT + "/" + request.Filename);
-                    }
                 }
-                // post processing pipe
+
                 foreach(KeyValuePair<string, PluginInfo> plugininfo in plugins) {
-                    if(plugininfo.Value.postprocessing) {
+                    if(plugininfo.Value.postprocessing)
                         response = plugininfo.Value.reference.PostProcessing(response);
-                    }
                 }
             }
-            // Generate response
+
             ns.Write(Encoding.UTF8.GetBytes(response.header), 0, response.header.Length);
-            if(response.body != null) {
-              ns.Write(response.body, 0, response.body.Length);
-            }
+            if(response.body != null)
+                ns.Write(response.body, 0, response.body.Length);
 
-            // Shuting down
-            //ns.Close();
             _client.Shutdown(SocketShutdown.Both);
-            //_client.Close();
-
         }
     }
 
     public class TaskInfo
     {
         private HTTPProcessor _hp;
-        public HTTPProcessor hp 
-        { 
-            get {return _hp;}
-            set {_hp = value;}
-        }
+        public HTTPProcessor hp { get {return _hp;} set {_hp = value;} }
+
         public TaskInfo(HTTPProcessor hp)
         {
             this.hp = hp;
         }
     }
 
-    /// <summary>
-    /// Main server class, open the socket and wait for client
-    /// </summary>
     public class DotNetWebServer
     {
         protected int _port;
@@ -255,17 +192,10 @@ namespace DNWS
             id = 0;
         }
 
-        /// <summary>
-        /// Singleton here
-        /// </summary>
-        /// <param name="parent">parent ref</param>
-        /// <returns></returns>
         public static DotNetWebServer GetInstance(Program parent)
         {
             if (_instance == null)
-            {
                 _instance = new DotNetWebServer(parent);
-            }
             return _instance;
         }
 
@@ -275,29 +205,30 @@ namespace DNWS
             ti.hp.Process();
         }
 
-        /// <summary>
-        /// Server starting point
-        /// </summary>
         public void Start()
         {
             _port = Convert.ToInt32(Program.Configuration["Port"]);
             string threadingMode = Program.Configuration["ThreadingMode"] ?? "Single";
             IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, _port);
-            // Create listening socket, queue size is 5 now.
+
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             serverSocket.Bind(localEndPoint);
             serverSocket.Listen(5);
+
             _parent.Log("Server started at port " + _port + " in " + threadingMode + " threading mode.");
+
             while (true)
             {
                 try
                 {
-                    // Wait for client
                     clientSocket = serverSocket.Accept();
-                    // Get one, show some info
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
+
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    hp.Process();
+                    TaskInfo ti = new TaskInfo(hp);
+
+                    Thread t = new Thread(new ParameterizedThreadStart(ThreadProc));
+                    t.Start(ti);
                 }
                 catch (Exception ex)
                 {
